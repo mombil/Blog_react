94180cdb9281cd5539edcdd0230f47e3e72e7ce3
Paweł Brejnak <pawelbrejnak6@gmail.com>
2024-01-15 19:34:31 +0100

    added theme

---
+++ src/theme.js
@@ -0,0 +1,11 @@
+export const theme = {
+    colors: {
+      primaryColor: "black",
+      secondaryColor: "white",
+    },
+    breakpoints: {
+      mediumDevices: "768px",
+      largeDevices: "992px",
+    },
+  };
+  
