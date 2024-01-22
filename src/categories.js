7796608b1ee30d5d2253c2eaf4d72f8983924165
mombil <pawelbrejnak6@gmail.com>
2024-01-22 16:27:27 +0100

    added file with all data

---
+++ src/categories.js
@@ -0,0 +1,165 @@
+export const categories = {
+    fps:{
+        title: "FPS",
+        description:  `
+        Osobę, która chce poczuć się jak główny bohater filmu akcji, na pewno zainteresują gry komputerowe typu FPS (z ang. First Person Shooter, czyli strzelanka pierwszoosobowa). Zazwyczaj celem w takich produkcjach jest pokonanie wroga podczas wykonywania misji. Żeby to osiągnąć, gracz ma do dyspozycji wachlarz broni palnej, miotanej lub białej. Wszystko, co dzieje się na ekranie, oglądasz oczami postaci. Pozwala to lepiej wczuć się w rozgrywkę, a także gwarantuje większy realizm walki – masz bowiem ograniczone pole widzenia.
+        
+        Przedstawiciel gatunku: Overwatch
+        Overwatch to flagowy FPS studia Blizzard Entertainment (znanego z tak dużych produkcji, jak z World of Warcraft czy Diablo). Nastawiony jest na tryb wieloosobowy, więc w celu rozegrania meczu konieczne będzie połączenie z Internetem. Rozgrywka polega na rywalizacji dwóch drużyn, a jej zasady zależą od ustalonego na początku meczu trybu gry.
+        
+        Gracz może wcielić się w jedną z wielu postaci z trzech różnych klas. Wybór determinuje rolę w zespole, którą należy wypełnić, by osiągnąć jak najlepszy rezultat. Każdy bohater ma unikatową osobowość, zestaw umiejętności i tzw. ultimate, czyli zdolność specjalną. Charakterystyczny element rozgrywki stanowi możliwość dynamicznej zmiany wybranego avatara na inny. Overwatch gwarantuje dynamiczną rywalizację, więc na pewno przypadnie do gustu graczom szukającym wrażeń.
+        `,
+        representatives:[
+            {
+                title: "Valorant",
+                img: "https://m.media-amazon.com/images/M/MV5BNmNhM2NjMTgtNmIyZC00ZmVjLTk4YWItZmZjNGY2NThiNDhkXkEyXkFqcGdeQXVyODU4MDU1NjU@._V1_FMjpg_UX1000_.jpg",
+            },
+            {
+                title: "Call of Duty Vanguard",
+                img: "https://m.media-amazon.com/images/M/MV5BMDQzMThkMGQtMGZlZi00ZmFiLTk5NzItYWJkYTAzNWUzMThhXkEyXkFqcGdeQXVyMTEzMTI1Mjk3._V1_.jpg",
+            },
+            {
+                title: "Doom Eternal",
+                img: "https://image.api.playstation.com/vulcan/ap/rnd/202010/0114/b4Q1XWYaTdJLUvRuALuqr0wP.png",
+            },
+        ],
+    },
+    rpg:{
+        title: "RPG",
+        description: `
+        Gra typu Role-Playing Game, czyli RPG, polega przede wszystkim na wcieleniu się w stworzonego bohatera poprzez wykonywanie zadań (questów), zgłębianie świata przedstawionego, rozwój umiejętności naszej postaci oraz podążaniu za linią fabularną. Bardzo często wybory, które zostaną przez Ciebie podjęte, mają wpływ na rozwój historii, co może prowadzić do jej kilku różnych zakończeń.
+
+        Od poszczególnych decyzji zależy też często kierunek rozwoju bohatera. Chyba nie ma lepszego sposobu, by przeżyć wirtualną przygodę, jeśli jesteś fanem długich i poruszających opowieści. Światy, w których dzieje się akcja najczęściej utrzymane są w konwencji fantasy, horroru lub science fiction, choć można spotkać gry praktycznie w każdym klimacie.
+        
+        Przedstawiciel gatunku: Wiedźmin 3: Dziki Gon
+        To kolejna odsłona serii przygód sławnego Geralta, czyli wiedźmina z książek Andrzeja Sapkowskiego – fantastycznej opowieści o łowcy potworów. Główny wątek tej części stanowi najazd Cesarstwa Nilfgaardu na Północne Królestwa. Gra oferuje otwarty świat, co oznacza brak sztywno określonej ścieżki postępu – zapewnia to graczowi ogromną dowolność w rozwoju historii i postaci. 
+        
+        Twórcy zadbali o to, by eksploracja świata nie była nudna: Dziki Gon ma aż 136 km². Bardzo dopracowano również animacje – sekwencje ruchu stały się o wiele płynniejsze, a mimika postaci jest dokładniejsza w porównaniu do drugiej części. To pozwala lepiej wczuć się w opowieść, co w przypadku gier RPG okazuje się rzeczą niezwykle ważną. Przyczyniło się to do pozytywnego odbioru gry w Polsce i na świecie – okrzyknięto ją najlepszą pozycją 2015 roku. 
+        
+        Ceną za tak dobrą jakość jest niestety konieczność posiadania wydajnej karty graficznej i całkiem sporej ilości RAM-u. Gra oferuje jednak aż 150 godzin rozrywki i dla fana fantastyki zdecydowanie będzie warta kosztów poniesionych na ewentualne ulepszenie komputera.`,
+        representatives:[
+            {
+                title: "Cyberpunk 2077",
+                img: "https://cdn.cloudflare.steamstatic.com/steam/apps/1091500/extras/616x670.jpg?t=1702306332",
+            },
+            {
+                title: "Dark Souls III",
+                img: "https://m.media-amazon.com/images/I/71bN7mqVC+L._AC_UF894,1000_QL80_.jpg",
+            },
+            {
+                title: "Mass Effect",
+                img: "https://upload.wikimedia.org/wikipedia/en/e/e8/MassEffect.jpg",
+            },
+        ],
+    },
+    zrecznosciowe:{
+        title: "Zręcznościowe",
+        description: `
+        Tytuły zręcznościowe to produkcje idealne dla osoby chcące się odstresować po ciężkim dniu. Najczęściej nie kładą nacisku na historię, więc nie doświadczysz w nich sytuacji, w której umknie Ci drobny, acz istotny dla fabuły szczegół. Chodzi bowiem głównie o satysfakcję płynącą z pokonywania kolejnych przeszkód, jakie oferują tego typu gry komputerowe.
+
+        Rodzaje wyzwań, które twórcy zręcznościówek stawiają przed graczami, są różne – od przejścia poziomu z wymyślnymi pułapkami, przez odnajdywanie rozmaitych przedmiotów, aż po szalone wyścigi na granicy praw fizyki. Jest to tak szeroka kategoria, że każdy znajdzie coś dla siebie.
+        
+        Przedstawiciel gatunku: Cuphead
+        Warto zwrócić uwagę na tę dwuwymiarową grę platformową. W dniu swojej premiery wywołała ona burzę w świecie graczy. Powód był prozaiczny – Cuphead jest po prostu bardzo trudny do przejścia. Mechanika przypomina Mario – możesz skakać, schylać się, chodzić w lewo i w prawo oraz strzelać. 
+        
+        Z pozoru dziecinnie proste sterowanie staje się zbawieniem na późniejszych etapach, ponieważ wraz z postępem zwiększa się poziom trudności walk z bossami, czyli głównymi przeciwnikami na danym etapie rozgrywki. Oczywiście dostajesz do dyspozycji różnego rodzaju ataki specjalne, jednak to od opanowania ruchu po planszy zależeć będzie, czy uda Ci się przeżyć.
+        
+        Dlaczego w takim razie jest to tak popularna gra? Powodem okazuje się jej warstwa wizualna. Masz bowiem tu do czynienia ze stylizacją na wczesne filmy Walta Disneya, która została okraszona niezwykle płynnymi animacjami. Całość sprawia wrażenie, jakby pochodziła z lat 20. XX wieku. 
+        
+        Cuphead to produkcja dostarczająca rozrywki nie tylko starym wyjadaczom gier zręcznościowych, ale także komuś, kto lubi wyzwania i chciałby spróbować czegoś nowego. A jeśli nadal masz problem z pokonaniem danego etapu, może to dobry moment, by rozejrzeć się za gamingową klawiaturą mechaniczną, która ułatwi Ci precyzyjniejsze sterowanie.`,
+        representatives:[
+            {
+                title: "Hollow Knight",
+                img: "https://cdn-products.eneba.com/resized-products/q7i-msiKJw8A73IeLKtYuwpr0ilw62NWSVQIFD0GbB0_350x200_2x-0.jpeg",
+            },
+            {
+                title: "Beat Saber",
+                img: "https://m.media-amazon.com/images/M/MV5BNjE2ZTRmZTAtOTBlNy00NGFiLThlZjctYmNjNWVjMjk0ZWYwXkEyXkFqcGdeQXVyOTU1NzIxMDg@._V1_.jpg",
+            },
+            {
+                title: "BirdGut",
+                img: "https://images.igdb.com/igdb/image/upload/t_cover_big_2x/co7h4j.jpg",
+            },
+        ],
+    },
+    przygodowe:{
+        title: "Przygodowe",
+        description: `
+        Kolejna kategoria powstała z myślą o osobach lubiących zagłębić się w fabułę i chcących utożsamić się z bohaterami, ale nie mających czasu na wielogodzinne rozgrywki RPG. Dobrze napisane postacie to trzon udanej gry przygodowej. Nie można jednoznacznie określić, jak wyglądają produkcje tego typu – mają bowiem bardzo zróżnicowany charakter. 
+
+        To, co je łączy, to odkrywanie i poznawanie świata przedstawionego, a także rozwiązywanie zawartych w nim zagadek. Jeśli gracz zaangażuje się emocjonalnie w ich rozwikłanie, będzie miał większą satysfakcję z przejścia pozycji do końca. To właśnie zadowolenie płynące z doświadczenia i zamknięcia historii jest powodem, dla którego gry przygodowe są tak popularne.
+        
+        Przedstawiciel gatunku: Night in the Woods
+        Jest to produkcja wyróżniająca się cechami gry przygodowej i platformowej. Gracz wciela się w Mae – sympatyczną kotkę, wracającą do swojej rodzinnej miejscowości, by odnowić relacje z dawnymi przyjaciółmi. Nostalgiczny klimat podupadającego miasteczka górniczego zostaje zaburzony, gdy bohaterka orientuje się, że pod jej nieobecność życie toczyło się dalej, jak gdyby nigdy nic. Jej przyjaciele rozpoczęli pracę i nie mają już czasu, aby spędzać całe dnie, bezcelowo włócząc się po okolicy. Mae dowiaduje się także, że jej kolega z czasów szkolnych zaginął, a jednocześnie z pobliskiego lasu zaczęły dochodzić dziwne odgłosy.
+        
+        Night in the Woods kładzie spory nacisk na narrację – duża część gry polega na rozmowie z napotkanymi mieszkańcami Possum Springs. To, w połączeniu z klimatyczną muzyką, sprawia, że Night in the Woods to bez wątpienia pozycja zapadająca w pamięć!`,
+        representatives:[
+            {
+                title: "Life Is Strange,",
+                img: "https://ih1.redbubble.net/image.504315885.7329/flat,750x,075,f-pad,750x1000,f8f8f8.u6.jpg",
+            },
+            {
+                title: "Machinarium",
+                img: "https://pics.filmaffinity.com/Machinarium-310919810-large.jpg",
+            },
+            {
+                title: "Detroit: Become Human",
+                img: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/45572318-b097-446c-9783-2b2ebb97c615/dcj5wvt-df42fd17-0f95-412d-ac08-d085970fc11f.png/v1/fill/w_900,h_1350,q_80,strp/detroit_become_human___movie_poster_by_abst3rgo_dcj5wvt-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTM1MCIsInBhdGgiOiJcL2ZcLzQ1NTcyMzE4LWIwOTctNDQ2Yy05NzgzLTJiMmViYjk3YzYxNVwvZGNqNXd2dC1kZjQyZmQxNy0wZjk1LTQxMmQtYWMwOC1kMDg1OTcwZmMxMWYucG5nIiwid2lkdGgiOiI8PTkwMCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.4pvn_exLalWRpmcBjAYQpN9tENgQ7CQONrznbSBMTak",
+            },
+        ],
+    },
+    battle_royale:{
+        title: "Battle Royale",
+        description: `
+        Większość gier z rodzaju Battle Royale odbywa się na podobnych zasadach – gracze transportowani są nad pole bitwy, by w odpowiednim dla siebie momencie wykonać skok z samolotu. Po wylądowaniu muszą jak najszybciej zdobyć przedmioty, które pozwalają na wydłużenie rozgrywki. Celem omawianego trybu jest bowiem przetrwanie – wygrywa ostatni żywy gracz na mapie.
+
+        Zwykle gry tego typu wyposażone są w pewnego rodzaju ograniczenia, na przykład zmniejszający się z czasem obszar walki. Rozwiązanie to ma zapobiec wydłużaniu się pojedynczej rundy. Osoby, które lubią wyzwania pod presją czasu, połączone z klimatem survivalu, na pewno pokochają tego typu strzelanki.
+        
+        Przedstawiciel gatunku: Playerunknown’s Battlegrounds
+        
+        PUBG, jak w skrócie nazywa się tę grę, cechuje się wysokim poziomem realizmu, co zdecydowanie odróżnia ją od innych przedstawicieli tej kategorii. Wcielasz się w zwykłego człowieka, zupełnie pozbawionego supermocy – każdy więc ma równe szanse na przetrwanie. 
+        
+        To od Ciebie zależy, w którym miejscu na mapie rozpoczniesz grę, a także jak dobrze zdołasz się dozbroić. Po krótkiej chwili od rozpoczęcia meczu obszar gry zaczyna się powoli kurczyć, co zmusza uczestników do poruszania się po mapie i odsłania ich na ataki ze strony innych.
+        
+        PUBG oferuje jedynie rozgrywkę sieciową, a na jednej mapie może przebywać aż 100 osób naraz, dzięki czemu podczas meczu nie da się narzekać na nudę. Ci, którzy nie lubią widoku pierwszoosobowego, mają do dyspozycji także perspektywę zza pleców swojego żołnierza. Gra jest niestety wymagająca pod względem technicznym: potrzebuje sporo pamięci RAM i wydajnej karty graficznej.`,
+        representatives:[
+            {
+                title: "Fortnite Battle Royale",
+                img: "https://i.ebayimg.com/images/g/MZYAAOSw7Cdfxvju/s-l1200.webp",
+            },
+            {
+                title: "Apex Legends",
+                img: "https://m.media-amazon.com/images/I/71MPnj8ZpNL._AC_UF894,1000_QL80_.jpg",
+            },
+            {
+                title: "Call of Duty: Warzone",
+                img: "https://myhotposters.com/cdn/shop/products/mL4593_1024x1024.jpg?v=1596632251",
+            },
+        ],
+    },
+    symulatory:{
+        title: "Symulatory",
+        description: `
+        Symulator to rodzaj gier komputerowych mających na celu najwierniejsze odwzorowanie rzeczywistości. Gracz za ich pośrednictwem ma okazję doświadczyć rzeczy, których niejednokrotnie nie mógłby wykonać w świecie rzeczywistym. Nowoczesne produkcje osiągają taki poziom realizmu, że wykorzystuje się je do ćwiczeń w poszczególnych sektorach przemysłu, na przykład symulator kokpitu samolotu wykorzystywany jest podczas szkolenia pilotów.
+
+        Przedstawiciel gatunku: The Sims 4
+        
+        Popularna seria The Sims to specjalny rodzaj symulatora, ponieważ nie skupia się na wybranej dziedzinie, a pozwala przeprowadzać symulację całego życia! Nie ma określonej linii fabularnej, nie posiada też głównych bohaterów – wszystko tworzy gracz. I to właśnie od niego zależy, jak przebiegnie życie jego podopiecznych (nazywanych Simami). Gra pozwala na naprawdę wiele – ma rozbudowany kreator postaci oraz domostw, a także wiele różnych opcji interakcji, za pomocą których wirtualni ludzie mogą się ze sobą komunikować.
+        
+        Czwarta część cyklu wprowadza ponadto emocje mające wpływ na rozwój i przebieg dnia podopiecznych. Istnieje opcja rozbudowania gry za pomocą poszczególnych dodatków. Sprzedawane są one jako osobne pakiety. Pod względem możliwości świata przedstawionego seria The Sims nie ma sobie równych.`,
+        representatives:[
+            {
+                title: "Euro Truck Simulator 2",
+                img: "https://howlongtobeat.com/games/Euro_Truck_Simulator_2_cover.jpg?width=250",
+            },
+            {
+                title: "Stardew Valley",
+                img: "https://i.etsystatic.com/19645555/r/il/f9a391/3205787491/il_fullxfull.3205787491_ay6k.jpg",
+            },
+            {
+                title: "Microsoft Flight Simulator",
+                img: "https://myhotposters.com/cdn/shop/products/mL4604_1024x1024.jpg?v=1596632286",
+            },
+        ],
+    },
+}
